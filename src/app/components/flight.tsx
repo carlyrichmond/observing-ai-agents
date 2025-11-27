fb7d53e31ca6483c24cacbb3b7a265c8172b256b
Carly Richmond <carly.richmond@elastic.co>
2025-11-27 11:32:36 +0000

    Fixing flight component

--- src/app/components/flight.tsx
+++ src/app/components/flight.tsx
@@ -10,6 +10,10 @@ export type RecommendedFlights = {
   inbound: Flight
 }
 
+export function priceWithCommas(price: number) {
+  return price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 export const Flights = (props: FlightProps) => {
   return (
     <div className="flights__summary__div">
@@ -18,12 +22,12 @@ export const Flights = (props: FlightProps) => {
           <h4>Outbound Flights</h4>
           { !props.outbound && <p>No return flights available</p> }
             <div className="flights__detail">
-              <p>{props.outbound.flight_number} on {new Date(props.outbound.departure_date).toLocaleDateString()}: £{props.outbound.price}</p>
+              <p>{props.outbound?.flight_number} on {new Date(props.outbound?.departure_date).toLocaleDateString()}: £{priceWithCommas(props.outbound?.price)}</p>
             </div>
           <h4>Inbound Flights</h4>
           { !props.inbound && <p>No return flights available</p> }
             <div className="flights__detail">
-              <p>{props.inbound.flight_number} on {new Date(props.outbound.departure_date).toLocaleDateString()}: £{props.inbound.price}</p>
+              <p>{props.inbound?.flight_number} on {new Date(props.inbound?.departure_date).toLocaleDateString()}: £{priceWithCommas(props.inbound?.price)}</p>
             </div>
         </div>
     </div>
